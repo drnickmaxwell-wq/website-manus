26ccc5a5c9a5837c18cd6bba786510b85440a1ac
nick maxwell <drnickmaxwell@gmail.com>
2025-10-19 22:05:18 -0700

    Delete app/treatments/general directory

--- app/treatments/general/sedation/page.tsx
+++
@@ -1,2 +0,0 @@
-export const metadata = { title: 'Sedation' };
-export default function Page(){return(<main style={{padding:'2rem'}}><h1>Sedation</h1><p>Placeholder page. Content coming shortly.</p></main>)}
